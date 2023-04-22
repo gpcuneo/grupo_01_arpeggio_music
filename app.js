f2d43975b6ef3e78386596cc94c086ef744dcdff
Guillermo Pedro Cuneo <guillermo.cuneo@cwc.com>
2023-04-22 17:28:07 -0300

    Se crearon las rutas para las vistas existentes hasta la fecha y se configuro la carpeta publica

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const path = require('path')
 
 const app = express();
-
 const port = 3000;
+
+const publicPath = path.resolve(__dirname, './public');
+app.use( express.static(publicPath) );
+
+const returnView = (req, res, viewName) => res.sendFile(path.resolve(__dirname, `./views/${viewName}.html`) );
+
 app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`) );
 
-app.get('/', (req, res) => res.send('Arpeggio Music - Sitio en construccion.') );
+app.get('/', (req, res) => returnView(req, res, 'index'));
+
+app.get('/productCart', (req, res) => returnView(req, res, req.path));
+
+app.get('/productDetail', (req, res) => returnView(req, res, req.path));
+
+app.get('/userLogin', (req, res) => returnView(req, res, req.path));
+
+app.get('/userProfile', (req, res) => returnView(req, res, req.path));
+
+app.get('/userRegister', (req, res) => returnView(req, res, req.path));
