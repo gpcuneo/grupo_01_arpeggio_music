571fe9cd162c56b1142d5c3084f4b1decc536526
Guillermo Pedro Cuneo <guillermo@cuneo.com.ar>
2023-05-21 15:29:44 -0300

    Se convirtio la home a ejs, se crearon 4 objetos del tipo producto

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,4 +1,12 @@
-const home = (req, res) => res.render('index');
+// Dummy data
+const products = [
+    {id: '1', name: 'Guittarra criolla', details: 'Guitarra electrica lorem lorem lorem', price: '274000', image: '/images/productos/guitarra02.png'},
+    {id: '2', name: 'Guitarra electrica', details: 'Guitarra electrica lorem lorem lorem', price: '310000', image: '/images/productos/guitarra01.png'},
+    {id: '3', name: 'Piano', details: 'Piano muchas teclasm lorem lorem lorem', price: '570342', image: '/images/productos/piano.png'},
+    {id: '4', name: 'Violin', details: 'Violin lorem lorem lorem lorem', price: '325738', image: '/images/productos/violin.png'}
+]
+
+const home = (req, res) => res.render('index', {products: products});
 
 const about = (req, res) => res.render('about');
 
