868882d31a8194e585334d81a109f00ea6223904
RBlytics <agus.rodbel@gmail.com>
2023-05-21 23:53:28 -0300

    Creación de crear/editar categorías: ejs, router, css y controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,13 +1,25 @@
 let showCategory = (req, res) => {
     res.render('category')
 }
+let getCategoryId = (req, res) => {
+    res.render('category')
+}
+let getCategoryCreate = (req, res) => {
+    res.render('category', {action:'create'})
+}
+let getCategoryUpDate = (req, res) => {
+    res.render('category', {action:'update'})
+}
+let getCategoryDelete = (req, res) => {
+    res.render('category')
+}
 
 const categoryController = {
-    show: showCategory,
-    // showbyid: '',
-    // create: '',
-    // update: '',
-    // delete: '',
+    show:showCategory,
+    showbyid:getCategoryId,
+    create:getCategoryCreate,
+    update:getCategoryUpDate,
+    delete:getCategoryDelete,
 }
 
 module.exports = categoryController
--- routes/category.js
+++ routes/category.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const category = require('../controllers/categoryController')
 
-router.get('/', category.show)
+router.use('/create', category.create);
+router.use('/:id?', category.showbyid)
+router.use('/id/update', category.update);
+router.use('/id/delete', category.delete)
+router.use('/', category.show);
 
 module.exports = router
