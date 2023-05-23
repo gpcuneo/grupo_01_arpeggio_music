d1e03885edbd79ae6e1fd2faac1cb6431272d54b
RBlytics <agus.rodbel@gmail.com>
2023-05-23 16:01:29 -0300

    Intento de pagina dinamica

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,27 @@
+// category data
+const categoryList = [
+    {id: '1', instrument: 'Guitarra criolla'},
+    {id: '2', instrument: 'Guitarra eléctrica'},
+    {id: '3', instrument: 'Piano'},
+    {id: '4', instrument: 'Batería'},
+    {id: '5', instrument: 'Teclado'},
+    {id: '6', instrument: 'Violín'},
+    {id: '7', instrument: 'Saxofón'},
+    {id: '8', instrument: 'Clarinete'},
+]
+
+
 let showCategory = (req, res) => {
-    res.render('category')
+    if(req.params.id){
+        console.log(req.params.id);
+    }
+    let categoryList = {}
+    categoryList.forEach( category['id'] === id ? categoryInfo = category : '');
+    console.log(categoryInfo);
+    res.render('category', {categoryInfo: categoryInfo})
+}
+const categoryUsers = (req, res) => {
+    res.render('categoryList', {'category': categoryList} );
 }
 let getCategoryId = (req, res) => {
     res.render('category')
