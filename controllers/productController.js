34a2432f29605fef9c78caebca0cfa53035f232e
unknown <rebeluna263@gmail.com>
2023-05-26 18:17:56 -0300

    se creo archivo json de articles y se modifico los atributos del objeto producto

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,56 +1,7 @@
-const product={
-    name:'Batería Acústica',
-    price:250000,
-    discount:20,
-    characteristics:'Batería Acústica 5 cuerpos 20 14 12 10 Yamaha Rydeen Rdp0f5',
-    stock: 1,
-    descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo nobis eveniet harum non, suscipit mollitia recusandae quidem blanditiis aperiam assumenda a corrupti quaerat enim fugiat eum veniam corporisminus quia.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem est nihil accusamus esse deserunt iste? Odio quo magni excepturi molestiae maiores cupiditate hic repellendus ipsum! Eius maxime ad assumenda provident',
-    store:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo nobis eveniet harum non, suscipit mollitia recusandae quidem blanditiis aperiam assumenda a corrupti quaerat enim fugiat eum veniam corporisminus quia.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem est nihil accusamus esse deserunt iste? Odio quo magni excepturi molestiae maiores cupiditate hic repellendus ipsum! Eius maxime ad assumenda provident',
-    img:[
-        {
-            img:'/images/productos/image-32.jpg'
-        },
-        {
-            img:'/images/productos/image-32.jpg'
-        },
-        {
-            img:'/images/productos/image-33.jpg'
-        },
-        {
-            img:'/images/productos/image-34.jpg'
-        },
-        {
-            img:'/images/productos/image-35.jpg'
-        },
-    ],
-}
+const readJSON = require('../utils/readJSON')
+let product = readJSON('productDetail.json');
+let articles = readJSON('articles.json');
 
-const articles=[
-    {
-        name:'Redoblante',
-        price:68800,
-        characteristics:'Redoblante Roadshow 14x6,5 8 Torres Rsn1465s',
-        img:'/images/productos/image-46.jpg'
-    },
-    {
-        name:'Pedal de bombo',
-        price:20000,
-        characteristics:'Pedal de Bombo Hebikuo G610 Cadena Doble base metalica',
-        img:'/images/productos/image-47.jpg'
-    },
-    {
-        name:'Bombo de Bateria',
-        price:100000,
-        characteristics:'Bombo de Bateria Pearl Forum Fzh2218b/c 22x18',
-        img:'/images/productos/image-48.jpg'
-    },
-    {
-        name:'Set de Platillos',
-        price:25000,
-        characteristics:'Set de platillos Zildjian Zp1418 Hi Hat 14 Cash 18 Palillos',
-        img:'/images/productos/image-49.jpg'
-    },
-]
 
 let getDetail=(req, res)=>{
     res.render('productDetail', {title:'Detalle del Producto',product, articles});
@@ -67,7 +18,7 @@ let getUpDate = (req, res)=>{
 }
 let getId = (req, res)=>{
     if(req.params.id){
-        console.log(req.params.id)
+        /* console.log(req.params.id) */
     }
     res.render('productDetail', {title:'Detalle del Producto',product, articles})
 }
