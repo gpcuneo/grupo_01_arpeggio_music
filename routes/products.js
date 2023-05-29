69f3d5af50292972c6ffc148dff14522c4eea9f1
Rebeca3Luna <rebeluna263@gmail.com>
2023-05-29 19:05:44 -0300

    se agrego unos comentarios en products como guía y se modifico el css de productDetail

--- routes/products.js
+++ routes/products.js
@@ -3,13 +3,25 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 
-
+//GET /products  
 router.get('/', productController.product);
+
+//POST /products 
 router.post('/', productController.postProduct);
+
+//GET /products/:id/detail
 router.get('/:id/detail', productController.showbyid);
+
+//GET /products/create
 router.get('/create', productController.create);
+
+//PUT /products/:id/update
 router.put('/:id/update', productController.update);
+
+//GET /products/:id/update
 router.get('/:id/update', productController.getUpDate);
+
+//DELETE /products/:id/delete 
 router.delete('/:id/delete', productController.delete);
 
 module.exports=router;
