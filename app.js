c36ed7e915b7e1f71160b0e69ac0e6f4c0dc8fe7
mgmaxi96 <maxi.mrtnz@gmail.com>
2023-06-12 21:43:03 -0300

    weekly

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const aboutRoutes = require('./routes/about');
 const cartRoutes = require('./routes/cart');
 const historyRoutes = require('./routes/history');
 const mainRoutes = require('./routes/main');
+const { log } = require('console');
 
 app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`) );
 
