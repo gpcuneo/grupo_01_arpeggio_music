2a5a2ddd003a415109fdce7bfc351a679c074e3a
Guillermo Pedro Cuneo <guillermo@cuneo.com.ar>
2023-06-18 12:49:15 -0300

    Limpieza: Se removieron console.log innecesarios

--- middlewares/authenticationsMiddleware.js
+++ middlewares/authenticationsMiddleware.js
@@ -1,7 +1,5 @@
 const jsonTools = require('../utils/JSONTools')
 const authenticationMiddleware = (req, res, next) => {
-    console.log(req.cookies)
-    console.log(req.cookies.userName)
     if(req.cookies.userName) {
         let users = jsonTools.read('users.json');
         let user = users.filter( ({userName}) => { return userName === req.cookies.userName });
@@ -10,7 +8,6 @@ const authenticationMiddleware = (req, res, next) => {
             delete user.id;
             delete user.password;
             req.session.user = user;
-            console.log('user is remember by cookie');
         }
     }
     
