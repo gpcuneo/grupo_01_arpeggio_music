3a4dbd51ae1a2b3f73e2d52876135e6a93a68872
unknown <rebeluna263@gmail.com>
2023-06-22 19:54:07 -0300

    Rutas protegidas donde solo el admin puede ingresar

--- routes/products.js
+++ routes/products.js
@@ -4,30 +4,31 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 //Middlewares
+const middlewares = require('../middlewares/index');
 const storageFile = require('../utils/storageTools');
 const upload = storageFile.upload('productos');
-const validateCreateMiddleware = require('../middlewares/index');
-
+const validateCreate = middlewares.validations;
+const isAdmin = middlewares.authorizationMiddleware.isAdmin;
 
 //GET /products  
-router.get('/', productController.product);
+router.get('/',isAdmin ,productController.product);
 
 //POST /products 
-router.post('/', upload.any('img'), validateCreateMiddleware.validations ,productController.postProduct);
+router.post('/', isAdmin,upload.any('img'), validateCreate,productController.postProduct);
 
 //GET /products/:id/detail
 router.get('/:id/detail', productController.showbyid);
 
 //GET /products/create
-router.get('/create', productController.create);
+router.get('/create', isAdmin ,productController.create);
 
 //PUT /products/:id/update
-router.put('/:id/update', productController.update);
+router.put('/:id/update', isAdmin,productController.update);
 
 //GET /products/:id/update
-router.get('/:id/update', productController.getUpDate);
+router.get('/:id/update',isAdmin ,productController.getUpDate);
 
 //DELETE /products/:id/delete 
-router.delete('/:id/delete', productController.delete);
+router.delete('/:id/delete', isAdmin,productController.delete);
 
 module.exports=router;
