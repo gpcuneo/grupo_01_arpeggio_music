d38c6433c1d63ddd596bcd6aeacefdfc83b5c4b6
unknown <rebeluna263@gmail.com>
2023-06-22 18:20:09 -0300

    se completo en la mayoría de los campos la validación de formulario de producto

--- middlewares/validateCreateProductMiddleware.js
+++ middlewares/validateCreateProductMiddleware.js
@@ -9,7 +9,15 @@ const validations = [
     body('stock').notEmpty().withMessage('Tienes que escribir el stock del producto'),
     body('colors').notEmpty().withMessage('Debes elegir un color'),
     body('characteristics').notEmpty().withMessage('Tienes que escribir las características del producto'),
-    body('description').notEmpty().withMessage('Tienes que escribir la descripción del producto')
+    body('description').notEmpty().withMessage('Tienes que escribir la descripción del producto'),
+    body('img').custom((value,{req})=>{
+        let files = req.files;
+
+        if(files.length == 0){
+            throw new Error('Tienes que subir como mínimo una imagen');
+        }
+        return true;
+    })
 ]
 
 module.exports = validations;
