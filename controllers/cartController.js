b9c5a7b3c1396b3da0f792b74c6b56a274f07aac
unknown <rebeluna263@gmail.com>
2023-06-29 20:15:22 -0300

    se agrego userInfo tambien en la vista del carrito

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,13 +1,14 @@
 const jsonTools= require('../utils/JSONTools');
 let cart = jsonTools.read('cart.json');
-
+const userTools = require('../utils/User')
 
 let getCart = (req, res)=>{
+    let userInfo = userTools.isLogged(req);
     const costShop={
         discount:25,
         costSend: 2500,
     }
-    res.render('productCart', {title:'Mi carrito', cart, costShop})
+    res.render('productCart', {title:'Mi carrito', cart, 'user':userInfo,costShop})
 }
 
 const cartController ={
--- routes/cart.js
+++ routes/cart.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 const cartController = require('../controllers/cartController');
 
-router.use('/', cartController.cart);
+router.get('/', cartController.cart);
 module.exports=router;
