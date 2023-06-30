43d2031ea94dd820ef694f0b5462e0feafb094ec
mgmaxi96 <maxi.mrtnz@gmail.com>
2023-06-30 11:17:52 -0300

    rutas de categoria protegidas

--- routes/category.js
+++ routes/category.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const categoryControllers = require('../controllers/categoryController');
 const multer = require ('multer')
 const categoryMiddleware = require ('../middlewares/validationsCategory')
+const indexAdmin = require ('../middlewares/index')
 const storage = multer.diskStorage({
     destination: (req, file , cb) =>{
         
@@ -17,24 +18,25 @@ const storage = multer.diskStorage({
 })
 const upload = multer ({storage})
 const validation = categoryMiddleware.validateCreateCategory 
+const isAdmin = indexAdmin.authorizationMiddleware.isAdmin
 
 //GET /category
-router.get('/', categoryControllers.getCategory)
+router.get('/',isAdmin ,categoryControllers.getCategory)
 //POST /category
 router.post('/',upload.single('img') ,validation ,categoryControllers.postCategory)
 //GET /category/create
 
-router.get('/create',categoryControllers.getCategoryCreate)
+router.get('/create',isAdmin,categoryControllers.getCategoryCreate)
 
 //GET /category/:id 
-router.get('/:id', categoryControllers.getCategoryId)
+router.get('/:id',isAdmin ,categoryControllers.getCategoryId)
 //POST /category/:id/delete
-router.get('/:id/delete', categoryControllers.getCategoryDelete1)
-router.delete('/:id', categoryControllers.getCategoryDelete2)
+router.get('/:id/delete',isAdmin ,categoryControllers.getCategoryDelete1)
+router.delete('/:id',isAdmin, categoryControllers.getCategoryDelete2)
 //GET /category/:id/update
-router.get('/:id/update', categoryControllers.getCategoryUpdate)
+router.get('/:id/update',isAdmin ,categoryControllers.getCategoryUpdate)
 //PUT /category/:id/update
-router.put('/:id/update', upload.single('img'),validation ,categoryControllers.putCategoryUpdate)
+router.put('/:id/update',upload.single('img'),validation ,isAdmin,categoryControllers.putCategoryUpdate)
 
 module.exports = router
 
