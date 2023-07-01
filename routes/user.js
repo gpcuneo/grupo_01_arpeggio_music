0b8af21583a726c74baeec2e9cba5a32764e61fb
Guillermo Pedro Cuneo <guillermo@cuneo.com.ar>
2023-07-01 14:47:12 -0300

    Se aplica el middleware de validacion de campos de usuario

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const middlewares = require('../middlewares/index');
 const isAdmin = middlewares.authorizationMiddleware.isAdmin;
 const isOwnerOrAdmin = middlewares.authorizationMiddleware.isOwnerOrAdmin;
 const isOwner = middlewares.authorizationMiddleware.isOwner;
+const validateUserFields = middlewares.validations.userFields;
 
 const storageFile = require('../utils/storageTools')
 const upload = storageFile.upload('userProfile');
@@ -12,13 +13,13 @@ const upload = storageFile.upload('userProfile');
 router.get('/login', userController.login);
 router.post('/login', userController.auth);
 router.get('/:userName/logout', isOwner, userController.logout);
-router.get('/register', userController.register);
+router.get('/register', validateUserFields, userController.register);
 router.get('/export', isAdmin, userController.export);
 router.get('/:userName/edit', isOwnerOrAdmin, userController.edit);
 router.get('/:userName/delete', isOwnerOrAdmin, userController.delete);
 router.post('/:userName/active', isOwnerOrAdmin, userController.enable);
 router.delete('/:userName', isOwnerOrAdmin, userController.disable);
-router.put('/:userName', isOwnerOrAdmin, userController.updateInfo);
+router.put('/:userName', isOwnerOrAdmin, validateUserFields,userController.updateInfo);
 router.put('/:userName/password', isOwnerOrAdmin, userController.updatePwd);
 router.put('/:userName/image', isOwnerOrAdmin, upload.single('userimage'), userController.updateImage);
 router.get('/:userName', isOwnerOrAdmin, userController.showByID);
