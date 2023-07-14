2d673e9a9305bd5781180aa314afdf2d5bfb2356
unknown <rebeluna263@gmail.com>
2023-07-14 20:16:26 -0300

    se creo el script de la tabla de Invoices

---
+++ database/scripts/Invoices.sql
@@ -0,0 +1,25 @@
+--
+-- Estructura de tabla para la tabla `Invoices`
+--
+
+DROP TABLE IF EXISTS `invoices`;
+CREATE TABLE IF NOT EXISTS `invoices`(
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `order_id` INT NOT NULL,
+    `sub_total` FLOAT NOT NULL,
+    `taxes` FLOAT NOT NULL,
+    `total` FLOAT NOT NULL,
+    `createdAt` DateTime NOT NULL,
+    `updatedAt` DateTime NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`order_id`) REFERENCES 'orders'(`id`),
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+--
+-- Volcar la base de datos para la tabla `invoices`
+--
+
+-- INSERT INTO `invoices` (`id`, `order_id`, `sub_total`, `taxes`,`total`,`createdAt`, `updatedAt`) VALUES
+-- (),
+-- (),
+-- (),
