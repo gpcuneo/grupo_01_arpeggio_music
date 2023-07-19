aca6e95ed14e09dd6702ffda777b0220275f3837
unknown <rebeluna263@gmail.com>
2023-07-19 13:00:04 -0300

    se agrego createdAt y updatdeAt en la tabla de colors

--- database/scripts/Colors.sql
+++ database/scripts/Colors.sql
@@ -5,20 +5,22 @@
 DROP TABLE IF EXISTS `colors`;
 CREATE TABLE IF NOT EXISTS `colors` (
     `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    `name` VARCHAR NOT NULL
-)ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+    `name` VARCHAR(40) NOT NULL,
+    `createdAt` DATETIME NOT NULL,
+    `updatedAt` DATETIME NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 --
 -- Volcar la base de datos para la tabla `colors`
 --
-
-INSERT INTO `colors` (`id`, `name`) VALUES
-(1, 'Verde'),
-(2, 'Rojo'),
-(3, 'Negro'),
-(4, 'Azul'),
-(5, 'Blanco'),
-(6, 'Gris'),
-(7, 'Amarrillo'),
-(8, 'Púrpura'),
-(9, 'Rosa'),
-(10, 'Marrón'),
+SET @datetimenow := NOW();
+INSERT INTO `colors` (`name`,`createdAt`, `updatedAt`) VALUES
+('Verde',@datetimenow,@datetimenow),
+('Rojo',@datetimenow,@datetimenow),
+('Negro',@datetimenow,@datetimenow),
+('Azul',@datetimenow,@datetimenow),
+('Blanco',@datetimenow,@datetimenow),
+('Gris',@datetimenow,@datetimenow),
+('Amarrillo',@datetimenow,@datetimenow),
+('Púrpura',@datetimenow,@datetimenow),
+('Rosa',@datetimenow,@datetimenow),
+('Marrón',@datetimenow,@datetimenow);
