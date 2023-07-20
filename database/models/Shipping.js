667cdd4cfc8ac11f61cadc59e1eaa57f3fa923e5
unknown <rebeluna263@gmail.com>
2023-07-20 16:32:54 -0300

    se creo el modelo de la tabla shippings

---
+++ database/models/Shipping.js
@@ -0,0 +1,37 @@
+module.exports=(sequelize, DataType)=>{
+    const alias = 'Shipping';
+
+    const cols= {
+        id:{
+            type: DataType.INTEGER,
+            primaryKey:true,
+            autoIncrement:true,
+            allowNull:false
+        },
+        status:{
+            type:DataType.STRING(200),
+            allowNull:false
+        },
+        order_id:{
+            type:DataType.INTEGER,
+            allowNull:false
+        },
+        createdAt: {
+            type: DataType.DATE,
+            allowNull: false,
+        },
+        updatedAt: {
+            type: DataType.DATE,
+            allowNull: false,
+        }
+    }
+
+    const config = {
+        tableName:'shippings',
+        timestamps:true
+    }
+
+    const Shipping = sequelize.define(alias, cols, config);
+
+    return Shipping;
+}
