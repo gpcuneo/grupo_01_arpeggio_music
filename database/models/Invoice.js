cab5ccc48bf247bd055321666b3cbe1d6221034a
unknown <rebeluna263@gmail.com>
2023-07-20 15:38:11 -0300

    se creo el modelo de la tabla de Invoices

---
+++ database/models/Invoice.js
@@ -0,0 +1,45 @@
+module.exports=(sequelize, DataType) =>{
+    const alias = 'Invoice';
+
+    const cols = {
+        id:{
+            type: DataType.INTEGER,
+            primaryKey: true,
+            allowNull:false,
+            autoIncrement:true
+        },
+        order_id:{
+            type:DataType.INTEGER,
+            allowNull:false
+        },
+        sub_total:{
+            type:DataType.FLOAT,
+            allowNull:false
+        },
+        taxes:{
+            type:DataType.FLOAT,
+            allowNull:false
+        },
+        total:{
+            type:DataType.FLOAT,
+            allowNull:false
+        },
+        createdAt: {
+            type: DataType.DATE,
+            allowNull: false,
+        },
+        updatedAt: {
+            type: DataType.DATE,
+            allowNull: false,
+        }
+    }
+
+    const config ={
+        tableName:'invoices',
+        timestamps:true,
+    }
+
+    const Invoice = sequelize.define(alias, cols, config);
+
+    return Invoice;
+}
