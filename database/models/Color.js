244bd1eca16a9542a01197abac51ae59b3bff768
unknown <rebeluna263@gmail.com>
2023-07-20 13:16:21 -0300

    asociación de color con la tabla de productos

--- database/models/Color.js
+++ database/models/Color.js
@@ -30,5 +30,15 @@ module.exports=(sequelize, DataType) =>{
 
     let Color = sequelize.define(alias, cols, config);
 
+    Color.associate=(models)=>{
+        Color.belongsToMany(models.Product,{
+            as:'products',
+            through:'product-color',
+            foreignKey:'color_id',
+            otherKey:'product_id',
+            timestamps:true
+        })
+    }
+
     return Color;
 }
