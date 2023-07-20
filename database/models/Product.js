ac9993aeb252eefa1322a26919801ace09cb943c
unknown <rebeluna263@gmail.com>
2023-07-20 12:22:07 -0300

    se creo el modelo de product

---
+++ database/models/Product.js
@@ -0,0 +1,58 @@
+module.exports = (sequelize, DataType) =>{
+    const alias = 'Product';
+
+    const cols = {
+        id:{
+            type:DataType.INTEGER,
+            primaryKey:true,
+            autoIncrement:true
+        },
+        name:{
+            type:DataType.STRING(35),
+            allowNull:false
+        },
+        characteristics:{
+            type:DataType.STRING(150),
+            allowNull:false
+        },
+        price:{
+            type:DataType.FLOAT,
+            allowNull:false
+        },
+        discount:{
+            type:DataType.FLOAT,
+            allowNull:false
+        },
+        category_id:{
+            type:DataType.INTEGER,
+            allowNull:false
+        },
+        description:{
+            type:DataType.TEXT,
+            allowNull:false
+        },
+        store:{
+            type:DataType.TEXT,
+            allowNull:true
+        },
+        image:{
+            type:DataType.STRING(255),
+            allowNull:false
+        },
+        createdAt: {
+            type: DataType.DATE,
+            allowNull: false,
+        },
+        updatedAt: {
+            type: DataType.DATE,
+            allowNull: false,
+        }
+    }
+
+    const config = {
+        tableName:'products',
+        timestamps:true,
+    }
+
+    return sequelize.define(alias, cols, config);
+}
