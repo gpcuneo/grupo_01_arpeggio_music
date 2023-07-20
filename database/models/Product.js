8e152dcaac74d654520bf45b10e6153280905b18
unknown <rebeluna263@gmail.com>
2023-07-20 12:59:25 -0300

    se agrego allowNull en el id del modelo de product

--- database/models/Product.js
+++ database/models/Product.js
@@ -5,6 +5,7 @@ module.exports = (sequelize, DataType) =>{
         id:{
             type:DataType.INTEGER,
             primaryKey:true,
+            allowNull:false,
             autoIncrement:true
         },
         name:{
