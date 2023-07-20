31f1cf95c406dc9c774bea1aa4d01665669e37be
unknown <rebeluna263@gmail.com>
2023-07-20 17:21:37 -0300

    se agrego la asociación de la tabla de order

--- database/models/Order.js
+++ database/models/Order.js
@@ -1,20 +1,20 @@
-module.exports=(sequelize, DataType) =>{
-    const alias ='Order';
+module.exports = (sequelize, DataType) => {
+    const alias = 'Order';
 
-    const cols={
-        id:{
-            type:DataType.INTEGER,
-            primaryKey:true,
-            autoIncrement:true,
-            allowNull:false
+    const cols = {
+        id: {
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
         },
-        user_id:{
-            type:DataType.CHAR(36),
-            allowNull:false
+        user_id: {
+            type: DataType.CHAR(36),
+            allowNull: false
         },
-        delivery_id:{
-            type:DataType.INTEGER,
-            allowNull:false
+        delivery_id: {
+            type: DataType.INTEGER,
+            allowNull: false
         },
         createdAt: {
             type: DataType.DATE,
@@ -25,14 +25,26 @@ module.exports=(sequelize, DataType) =>{
             allowNull: false,
         }
 
-    }
+    };
 
-    const config ={
-        tableName:'orders',
-        timestamps:true
-    }
+    const config = {
+        tableName: 'orders',
+        timestamps: true
+    };
 
     const Order = sequelize.define(alias, cols, config);
+
+    Order.associate = models => {
+        Order.belongsTo(models.User, {
+            as: 'user',
+            foreignKey: 'user_id'
+        });
+        Order.belongsTo(models.Delivery, {
+            as: 'delivery',
+            foreignKey: 'delivery_id'
+        })
+    };
+
 
     return Order;
 }
