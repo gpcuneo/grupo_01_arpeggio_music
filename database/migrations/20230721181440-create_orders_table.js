8ea11a04630e1aad32849b5a3113f2d2f0c70d1f
unknown <rebeluna263@gmail.com>
2023-07-21 15:17:21 -0300

    se creo la migracion de la tabla de orders

---
+++ database/migrations/20230721181440-create_orders_table.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('orders', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      user_id: {
+        type: Sequelize.CHAR(36),
+        allowNull: false
+      },
+      delivery_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('orders');
+    
+  }
+};
