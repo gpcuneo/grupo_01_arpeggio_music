d78f41151a0ebbba80a88dc04fd9c2ee3ff7e9f1
Guillermo Pedro Cuneo <guillermo@cuneo.com.ar>
2023-07-29 11:31:07 -0300

    Control sobre seeders ya ejecutados para evitar duplicidad de carga de datos

--- database/seeders/20230723193123-add-roles.js
+++ database/seeders/20230723193123-add-roles.js
@@ -1,14 +1,28 @@
 'use strict';
-
+var path = require('path');
+const loadStatus = require('./check_load')
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    const dateNow = new Date();
-    await queryInterface.bulkInsert('roles', [
-      {type: 'user', createdAt: dateNow, updatedAt: dateNow},
-      {type: 'admin', createdAt: dateNow, updatedAt: dateNow},
-      {type: 'editor', createdAt: dateNow, updatedAt: dateNow}
-    ], {});
+    var fileName = path.basename(__filename);
+    const isInitialized = await loadStatus.checkIfInitialized(fileName);
+    if (!isInitialized) {
+        try {
+            const dateNow = new Date();
+            await queryInterface.bulkInsert('roles', [
+              {type: 'user', createdAt: dateNow, updatedAt: dateNow},
+              {type: 'admin', createdAt: dateNow, updatedAt: dateNow},
+              {type: 'editor', createdAt: dateNow, updatedAt: dateNow}
+            ], {});
+            console.log('Datos iniciales insertados con éxito.');
+
+            await loadStatus.markAsInitialized(fileName);
+        } catch (error) {
+            console.error('Error al insertar datos iniciales:', error);
+        }
+    } else {
+        console.log('La inicialización ya se ha realizado previamente.');
+    }
   },
 
   async down (queryInterface, Sequelize) {
--- database/seeders/20230725235916-add-deliveries.js
+++ database/seeders/20230725235916-add-deliveries.js
@@ -1,20 +1,32 @@
 'use strict';
-
+var path = require('path');
+const loadStatus = require('./check_load')
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    const dateNow = new Date();
-    await queryInterface.bulkInsert ('deliveries',[
-    {type:'Retiro en el local',createdAt:dateNow,updatedAt:dateNow},
-    {type:'OCA', createdAt:dateNow,updatedAt:dateNow},
-    {type:'Andreani', createdAt:dateNow, updatedAt:dateNow},
-    {type:'Mensajeria privada', createdAt:dateNow, updatedAt:dateNow},
-    ],{})
+    var fileName = path.basename(__filename);
+    const isInitialized = await loadStatus.checkIfInitialized(fileName);
+    if (!isInitialized) {
+        try {
+            const dateNow = new Date();
+            await queryInterface.bulkInsert ('deliveries',[
+              {type:'Retiro en el local',createdAt:dateNow,updatedAt:dateNow},
+              {type:'OCA', createdAt:dateNow,updatedAt:dateNow},
+              {type:'Andreani', createdAt:dateNow, updatedAt:dateNow},
+              {type:'Mensajeria privada', createdAt:dateNow, updatedAt:dateNow},
+              ],{});
+            console.log('Datos iniciales insertados con éxito.');
+
+            await loadStatus.markAsInitialized(fileName);
+        } catch (error) {
+            console.error('Error al insertar datos iniciales:', error);
+        }
+    } else {
+        console.log('La inicialización ya se ha realizado previamente.');
+    }
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('deliveries',null,{})
+    await queryInterface.bulkDelete('v', null, {});
   }
 };
-
-
