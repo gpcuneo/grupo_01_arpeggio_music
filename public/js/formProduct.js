dbb8ee67a3b7243c4079681e2a86f82762c647db
unknown <rebeluna263@gmail.com>
2023-08-11 20:27:18 -0300

    coloque todos las validaciones dentro de DOMContentLoaded y agregue el evento submit para el form de create

--- public/js/formProduct.js
+++ public/js/formProduct.js
@@ -1,20 +1,17 @@
-const nameInput = document.getElementById('name');
-const categorySelect = document.getElementById('category');
-const priceInput = document.getElementById('price');
-/* const discountSelect = document.getElementById('discount'); */
-/* const trademarkSelect = document.getElementById('trademark'); */
-const stockInput = document.getElementById('stock');
-const colorsCheck = Array.from(document.querySelectorAll('input[type="checkbox"]'));
-const imageInput = document.querySelector('input[type="file"]');
-const characterInput = document.getElementById('characteristics');
-const descriptionInput = document.getElementById('description');
-const storeInput = document.getElementById('store');
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('form');
+    const nameInput = document.getElementById('name');
+    const categorySelect = document.getElementById('category');
+    const priceInput = document.getElementById('price');
+    const stockInput = document.getElementById('stock');
+    const colorsCheck = Array.from(document.querySelectorAll('input[type="checkbox"]'));
+    const imageInput = document.querySelector('input[type="file"]');
+    const characterInput = document.getElementById('characteristics');
+    const descriptionInput = document.getElementById('description');
+    const storeInput = document.getElementById('store');
+    const submitForm = document.querySelector('#submit-btn');
 
-
-const checkErrors = ()=>{
-
-}
-nameInput.oninput =(e)=>{
+    nameInput.oninput =(e)=>{
         const value = e.target.value;
         const length = e.target.value.length;
         const showP = document.getElementById('pName');
@@ -28,127 +25,157 @@ nameInput.oninput =(e)=>{
             showP.style.display='none';
             e.target.nextElementSibling.innerHTML= '';
         }
-        
-}
-priceInput.oninput =(e)=>{
-    const value = e.target.value;
-    const length = e.target.value.length;
-    const showP = document.getElementById('p-price');
-    if(length == 0 ){
-        showP.style.display = 'block';
-        e.target.nextElementSibling.innerHTML = 'Debe agregar un precio al producto'
-    }else{
-        showP.style.display='none';
-        e.target.nextElementSibling.innerHTML= '';
+        checkErrors();
     }
-}
-stockInput.oninput =(e)=>{
-    const value = e.target.value;
-    const length = e.target.value.length;
-    const showP = document.getElementById('p-stock');
-    if(length == 0 ){
-        showP.style.display = 'block';
-        e.target.nextElementSibling.innerHTML = 'Debe agregar el stock del producto'
-    }else{
-        showP.style.display='none';
-        e.target.nextElementSibling.innerHTML= '';
-    }
-}
-categorySelect.onchange=(e)=>{
-    const value = e.target.value;
-    const length = e.target.value.length;
-    const showP = document.getElementById('p-category')
-    if(length == 0 ){
-        showP.style.display = 'block';
-        e.target.nextElementSibling.innerHTML = 'Elija una category'
-    }else{
-        showP.style.display='none';
-        e.target.nextElementSibling.innerHTML= '';
-    }
-}
-const checkColorArr = [] 
-const showPcolor = document.getElementById('p-color');
-colorsCheck.forEach(color =>{
-    color.onchange=(e)=>{
-        const colorId = color.id;
+    priceInput.oninput =(e)=>{
         const value = e.target.value;
-        if(colorId && value){
-            if(e.target.checked){
-                checkColorArr.push(value);
-                showPcolor.style.display= 'none';
-                showPcolor.innerHTML = ''
-            }else{
-                const index = checkColorArr.indexOf(value);
-                if(index !== -1){
-                    checkColorArr.splice(index, 1)
-                }
-                if(checkColorArr.length == 0){
-                    showPcolor.style.display = 'block';
-                    showPcolor.innerHTML = 'Debe Eligir un color'               
+        const length = e.target.value.length;
+        const showP = document.getElementById('p-price');
+        if(length == 0 ){
+            showP.style.display = 'block';
+            e.target.nextElementSibling.innerHTML = 'Debe agregar un precio al producto'
+        }else{
+            showP.style.display='none';
+            e.target.nextElementSibling.innerHTML= '';
+        }
+        checkErrors();
+    }
+    stockInput.oninput =(e)=>{
+        const value = e.target.value;
+        const length = e.target.value.length;
+        const showP = document.getElementById('p-stock');
+        if(length == 0 ){
+            showP.style.display = 'block';
+            e.target.nextElementSibling.innerHTML = 'Debe agregar el stock del producto'
+        }else{
+            showP.style.display='none';
+            e.target.nextElementSibling.innerHTML= '';
+        }
+        checkErrors();
+    }
+    categorySelect.onchange=(e)=>{
+        const value = e.target.value;
+        const length = e.target.value.length;
+        const showP = document.getElementById('p-category')
+        if(length == 0 ){
+            showP.style.display = 'block';
+            e.target.nextElementSibling.innerHTML = 'Elija una category'
+        }else{
+            showP.style.display='none';
+            e.target.nextElementSibling.innerHTML= '';
+        }
+        checkErrors();
+    }
+    const checkColorArr = [] 
+    const showPcolor = document.getElementById('p-color');
+    colorsCheck.forEach(color =>{
+        color.onchange=(e)=>{
+            const colorId = color.id;
+            const value = e.target.value;
+            if(colorId && value){
+                if(e.target.checked){
+                    checkColorArr.push(value);
+                    showPcolor.style.display= 'none';
+                    showPcolor.innerHTML = ''
+                }else{
+                    const index = checkColorArr.indexOf(value);
+                    if(index !== -1){
+                        checkColorArr.splice(index, 1)
+                    }
+                    if(checkColorArr.length == 0){
+                        showPcolor.style.display = 'block';
+                        showPcolor.innerHTML = 'Debe Eligir un color'               
+                    }
                 }
             }
+            checkErrors();
         }
-    }
-})
-imageInput.oninput=(e)=>{
-    const value = e.target.value;
-    const length= e.target.value.length;
-    const showP = document.getElementById('p-image');
-    if(value == ''){
-        showP.style.display = 'block';
-        e.target.nextElementSibling.innerHTML='No ha seleccionado una imagen'
-    }else{
-        let ArrExtension = ['jpg','jpeg', 'png'];
-        let extension = value.replace(/^.*\./, '');
-        extension= extension.toLowerCase();
-        if(ArrExtension.includes(extension)){
-            showP.style.display= 'none';
-            e.target.nextElementSibling.innerHTML = '';
+    })
+    imageInput.oninput=(e)=>{
+        const value = e.target.value;
+        const length= e.target.value.length;
+        const showP = document.getElementById('p-image');
+        if(value == ''){
+            showP.style.display = 'block';
+            e.target.nextElementSibling.innerHTML='No ha seleccionado una imagen'
         }else{
-            showP.style.display= 'block';
-            e.target.nextElementSibling.innerHTML = 'Solo se permite jpg, jpeg, png.'
+            let ArrExtension = ['jpg','jpeg', 'png'];
+            let extension = value.replace(/^.*\./, '');
+            extension= extension.toLowerCase();
+            if(ArrExtension.includes(extension)){
+                showP.style.display= 'none';
+                e.target.nextElementSibling.innerHTML = '';
+            }else{
+                showP.style.display= 'block';
+                e.target.nextElementSibling.innerHTML = 'Solo se permite jpg, jpeg, png.'
+            }
+        }
+        checkErrors();
+    }
+    characterInput.oninput =(e)=>{
+        const value = e.target.value;
+        const length = e.target.value.length;
+        const showP = document.getElementById('p-charater');
+        if(length == 0 ){
+            showP.style.display = 'block';
+            e.target.nextElementSibling.innerHTML = 'Agregue una característica del producto'
+        }else if(length > 150){
+            showP.style.display='block';
+            e.target.nextElementSibling.innerHTML = 'El texto no debe superar los 150 caracteres'
+        }else{
+            showP.style.display='none';
+            e.target.nextElementSibling.innerHTML= '';
+        }
+        checkErrors();
+    }
+    descriptionInput.oninput =(e)=>{
+        const value = e.target.value;
+        const length = e.target.value.length;
+        const showP = document.getElementById('p-description');
+        if(length == 0 ){
+            showP.style.display = 'block';
+            e.target.nextElementSibling.innerHTML = 'Agregue una descripción del producto'
+        }else if(length > 490){
+            showP.style.display='block';
+            e.target.nextElementSibling.innerHTML = 'El texto no debe superar los 490 caracteres'
+        }else{
+            showP.style.display='none';
+            e.target.nextElementSibling.innerHTML= '';
+        }
+        checkErrors();
+    }
+    storeInput.oninput =(e)=>{
+        const value = e.target.value;
+        const length = e.target.value.length;
+        const showP = document.getElementById('p-store');
+        if(length > 490){
+            showP.style.display='block';
+            e.target.nextElementSibling.innerHTML = 'El texto no debe superar los 490 caracteres'
+        }else{
+            showP.style.display='none';
+            e.target.nextElementSibling.innerHTML= '';
+        }
+        checkErrors();
+    }
+
+    const checkErrors = ()=>{
+        let errorsHTML= Array.from(document.querySelectorAll('.errors'))
+        let errors = [];
+        errorsHTML.forEach(error =>{
+            if(error.innerHTML !== ''){
+                errors.push(error.innerHTML);
+            }
+        })
+        if(errors.length > 0 ){
+            submitForm.disabled= true;
+        }else{
+            submitForm.removeAttribute('disabled')
+            submitForm.style.backgroundColor='black'
         }
     }
-}
-characterInput.oninput =(e)=>{
-    const value = e.target.value;
-    const length = e.target.value.length;
-    const showP = document.getElementById('p-charater');
-    if(length == 0 ){
-        showP.style.display = 'block';
-        e.target.nextElementSibling.innerHTML = 'Agregue una característica del producto'
-    }else if(length > 150){
-        showP.style.display='block';
-        e.target.nextElementSibling.innerHTML = 'El texto no debe superar los 150 caracteres'
-    }else{
-        showP.style.display='none';
-        e.target.nextElementSibling.innerHTML= '';
-    }
-}
-descriptionInput.oninput =(e)=>{
-    const value = e.target.value;
-    const length = e.target.value.length;
-    const showP = document.getElementById('p-description');
-    if(length == 0 ){
-        showP.style.display = 'block';
-        e.target.nextElementSibling.innerHTML = 'Agregue una descripción del producto'
-    }else if(length > 490){
-        showP.style.display='block';
-        e.target.nextElementSibling.innerHTML = 'El texto no debe superar los 490 caracteres'
-    }else{
-        showP.style.display='none';
-        e.target.nextElementSibling.innerHTML= '';
-    }
-}
-storeInput.oninput =(e)=>{
-    const value = e.target.value;
-    const length = e.target.value.length;
-    const showP = document.getElementById('p-store');
-    if(length > 490){
-        showP.style.display='block';
-        e.target.nextElementSibling.innerHTML = 'El texto no debe superar los 490 caracteres'
-    }else{
-        showP.style.display='none';
-        e.target.nextElementSibling.innerHTML= '';
-    }
-}
+    form.addEventListener('submit', function(event){
+        if(!checkErrors){
+            event.preventDefault();
+        }
+    })
+})
