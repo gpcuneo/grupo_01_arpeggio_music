2bdec1e25eb4660c010421d124e1f0598ac0612c
mgmaxi96 <maxi.mrtnz@gmail.com>
2023-08-15 15:48:47 -0300

    realice la validacion de categorias

---
+++ public/js/Category.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const text = document.querySelector('.text')
+    const img = document.querySelector('#img')
+    text.oninput = (e) => {
+        const value = e.target.value
+        const lenght = e.target.value.length
+        const showP = document.getElementById('p-name')
+      if(lenght===0){
+        showP.style.display='block'
+        e.target.nextElementSibling.innerHTML = 'El texto debe contener 5 caracteres'
+      } else if (lenght<4){
+        showP.style.display='block'
+        e.target.nextElementSibling.innerHTML = 'El texto debe contener 5 caracteres'
+      } else if (lenght >25){
+        showP.style.display='block'
+        e.target.nextElementSibling.innerHTML = 'El texto debe contener 5 caracteres'
+    } else {
+showP.style.display = 'none'
+e.target.nextElementSibling.innerHTML = ''
+    }
+    checkErrors ()
+}
+img.oninput = (e)=>{
+    const value = e.target.value
+    const lenght = e.target.value.length
+    const showP = document.getElementById('p-img')
+if (value == ''){
+    showP.style.display='block'
+    e.target.nextElementSibling.innerHTML = 'Debe cargar una imagen'
+}
+checkErrors()
+}
+
+const checkErrors = () =>{
+let errorsHTML =Array.from (document.querySelectorAll('.error'))
+let errors = []
+console.log(errorsHTML);
+errorsHTML.forEach (error =>{
+    if (error.innerHTML !== ''){
+        errors.push (error.innerHTML)
+        
+    }
+
+    
+    
+})
+
+
+
+
+
+}
+
+})
+
+
