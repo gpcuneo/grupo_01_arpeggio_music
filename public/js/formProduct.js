e308f2c178331d0018644e3910622f617729e5f6
unknown <rebeluna263@gmail.com>
2023-08-15 21:35:22 -0300

    detalles finales de validación del lado del frontend

--- public/js/formProduct.js
+++ public/js/formProduct.js
@@ -11,19 +11,32 @@ document.addEventListener('DOMContentLoaded', function () {
     const storeInput = document.getElementById('store');
     const submitForm = document.querySelector('#submit-btn');
 
+    const formFilesValidated = {
+        name:false,
+        category:false,
+        price:false,
+        stock:false,
+        colors:false,
+        image:false,
+        character:false,
+        description:false,
+        store:true
+    }
     nameInput.oninput =(e)=>{
-        const value = e.target.value;
         const length = e.target.value.length;
         const showP = document.getElementById('pName');
         if(length < 3 ){
+            formFilesValidated.name = false
             nameInput.classList.add('invalid')
             showP.style.display = 'block';
             e.target.nextElementSibling.innerHTML = 'Debe tener al menos 3 caracteres';
         }else if(length > 35){
+            formFilesValidated.name = false
             nameInput.classList.add('invalid')
             showP.style.display='block';
             e.target.nextElementSibling.innerHTML = 'El nombre no debe superar los 35 caracteres'
         }else{
+            formFilesValidated.name = true
             nameInput.classList.remove('invalid')
             showP.style.display='none';
             e.target.nextElementSibling.innerHTML= '';
@@ -31,14 +44,15 @@ document.addEventListener('DOMContentLoaded', function () {
         checkErrors();
     }
     priceInput.oninput =(e)=>{
-        const value = e.target.value;
         const length = e.target.value.length;
         const showP = document.getElementById('p-price');
         if(length == 0 ){
+            formFilesValidated.price = false
             priceInput.classList.add('invalid')
             showP.style.display = 'block';
             e.target.nextElementSibling.innerHTML = 'Debe agregar un precio al producto'
         }else{
+            formFilesValidated.price = true
             priceInput.classList.remove('invalid')
             showP.style.display='none';
             e.target.nextElementSibling.innerHTML= '';
@@ -46,14 +60,15 @@ document.addEventListener('DOMContentLoaded', function () {
         checkErrors();
     }
     stockInput.oninput =(e)=>{
-        const value = e.target.value;
         const length = e.target.value.length;
         const showP = document.getElementById('p-stock');
         if(length == 0 ){
+            formFilesValidated.stock = false
             stockInput.classList.add('invalid')
             showP.style.display = 'block';
             e.target.nextElementSibling.innerHTML = 'Debe agregar el stock del producto'
         }else{
+            formFilesValidated.stock = true
             stockInput.classList.remove('invalid')
             showP.style.display='none';
             e.target.nextElementSibling.innerHTML= '';
@@ -61,14 +76,15 @@ document.addEventListener('DOMContentLoaded', function () {
         checkErrors();
     }
     categorySelect.onchange=(e)=>{
-        const value = e.target.value;
         const length = e.target.value.length;
         const showP = document.getElementById('p-category')
         if(length == 0 ){
+            formFilesValidated.category = false
             categorySelect.classList.add('invalid')
             showP.style.display = 'block';
-            e.target.nextElementSibling.innerHTML = 'Elija una category'
+            e.target.nextElementSibling.innerHTML = 'Elija una categoria'
         }else{
+            formFilesValidated.category = true
             categorySelect.classList.remove('invalid')
             showP.style.display='none';
             e.target.nextElementSibling.innerHTML= '';
@@ -83,6 +99,7 @@ document.addEventListener('DOMContentLoaded', function () {
             const value = e.target.value;
             if(colorId && value){
                 if(e.target.checked){
+                    formFilesValidated.colors = true
                     checkColorArr.push(value);
                     showPcolor.style.display= 'none';
                     showPcolor.innerHTML = ''
@@ -92,6 +109,7 @@ document.addEventListener('DOMContentLoaded', function () {
                         checkColorArr.splice(index, 1)
                     }
                     if(checkColorArr.length == 0){
+                        formFilesValidated.colors = false
                         showPcolor.style.display = 'block';
                         showPcolor.innerHTML = 'Debe Eligir un color'               
                     }
@@ -102,9 +120,9 @@ document.addEventListener('DOMContentLoaded', function () {
     })
     imageInput.oninput=(e)=>{
         const value = e.target.value;
-        const length= e.target.value.length;
         const showP = document.getElementById('p-image');
         if(value == ''){
+            formFilesValidated.image = false
             showP.style.display = 'block';
             e.target.nextElementSibling.innerHTML='No ha seleccionado una imagen'
         }else{
@@ -112,9 +130,11 @@ document.addEventListener('DOMContentLoaded', function () {
             let extension = value.replace(/^.*\./, '');
             extension= extension.toLowerCase();
             if(ArrExtension.includes(extension)){
+                formFilesValidated.image = true
                 showP.style.display= 'none';
                 e.target.nextElementSibling.innerHTML = '';
             }else{
+                formFilesValidated.image = false
                 showP.style.display= 'block';
                 e.target.nextElementSibling.innerHTML = 'Solo se permite jpg, jpeg, png.'
             }
@@ -122,22 +142,25 @@ document.addEventListener('DOMContentLoaded', function () {
         checkErrors();
     }
     characterInput.oninput =(e)=>{
-        const value = e.target.value;
         const length = e.target.value.length;
         const showP = document.getElementById('p-charater');
         if(length == 0 ){
+            formFilesValidated.character = false
             characterInput.classList.add('invalid')
             showP.style.display = 'block';
             e.target.nextElementSibling.innerHTML = 'Agregue una característica del producto'
         }else if(length > 150){
+            formFilesValidated.character = false
             characterInput.classList.add('invalid')
             showP.style.display='block';
             e.target.nextElementSibling.innerHTML = 'El texto no debe superar los 150 caracteres'
         }else if(length < 40){
+            formFilesValidated.character = false
             characterInput.classList.add('invalid')
             showP.style.display='block';
             e.target.nextElementSibling.innerHTML = 'El texto debe tener como mínimo 40 caracteres'
         }else{
+            formFilesValidated.character = true
             characterInput.classList.remove('invalid')
             showP.style.display='none';
             e.target.nextElementSibling.innerHTML= '';
@@ -145,22 +168,25 @@ document.addEventListener('DOMContentLoaded', function () {
         checkErrors();
     }
     descriptionInput.oninput =(e)=>{
-        const value = e.target.value;
         const length = e.target.value.length;
         const showP = document.getElementById('p-description');
         if(length == 0 ){
+            formFilesValidated.description = false
             descriptionInput.classList.add('invalid')
             showP.style.display = 'block';
             e.target.nextElementSibling.innerHTML = 'Agregue una descripción del producto'
         }else if(length > 490){
+            formFilesValidated.description = false
             descriptionInput.classList.add('invalid')
             showP.style.display='block';
             e.target.nextElementSibling.innerHTML = 'El texto no debe superar los 490 caracteres'
         }else if(length < 100){
+            formFilesValidated.description = false
             descriptionInput.classList.add('invalid')
             showP.style.display='block';
             e.target.nextElementSibling.innerHTML = 'El texto debe tener como mínimo 100 caracteres'
         }else{
+            formFilesValidated.description = true
             descriptionInput.classList.remove('invalid')
             showP.style.display='none';
             e.target.nextElementSibling.innerHTML= '';
@@ -168,38 +194,36 @@ document.addEventListener('DOMContentLoaded', function () {
         checkErrors();
     }
     storeInput.oninput =(e)=>{
-        const value = e.target.value;
         const length = e.target.value.length;
         const showP = document.getElementById('p-store');
         if(length > 490){
+            formFilesValidated.store = false
             storeInput.classList.add('invalid')
             showP.style.display='block';
             e.target.nextElementSibling.innerHTML = 'El texto no debe superar los 490 caracteres'
         }else {
+            formFilesValidated.store = true
             storeInput.classList.remove('invalid')
             showP.style.display='none';
             e.target.nextElementSibling.innerHTML= '';
         }
         checkErrors();
     }
-
     const checkErrors = ()=>{
-        let errorsHTML= Array.from(document.querySelectorAll('.errors'))
-        let errors = [];
-        errorsHTML.forEach(error =>{
-            if(error.innerHTML !== ''){
-                errors.push(error.innerHTML);
+        for(let key in formFilesValidated){
+            if(!formFilesValidated.hasOwnProperty(key))continue;
+            if(typeof formFilesValidated[key] !== 'boolean' || formFilesValidated[key] !== true){
+                return false;
             }
-        })
-        if(errors.length > 0 ){
-            submitForm.disabled= true;
-        }else{
-            submitForm.removeAttribute('disabled')
-            submitForm.style.backgroundColor='black'
         }
+        return true;
     }
-    form.addEventListener('submit', function(event){
-        if(!checkErrors){
+    form.addEventListener('change', function(event){
+        if(checkErrors()){
+            submitForm.disabled = false;
+            submitForm.style.backgroundColor='black'
+        }else{
+            submitForm.disabled = true;
             event.preventDefault();
         }
     })
