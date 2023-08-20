9df4b11a1de1c762b0cdcaefc194fb8c02e44436
Guillermo Pedro Cuneo <guillermo@cuneo.com.ar>
2023-08-20 18:15:16 -0300

    Se corrige filtro de usuario en la busqueda via api

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,6 @@ const apiController = require('../controllers/apiController');
 router.get('/town/:id', apiController.getTowns)
 router.get('/email/:email', apiController.checkEmail)
 router.get('/users/', apiController.userList)
-router.get('/users/:id', apiController.userDetail)
+router.get('/users/:userName', apiController.userDetail)
 
 module.exports=router;
