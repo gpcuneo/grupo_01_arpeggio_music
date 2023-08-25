9f1f73d80b10aadfc0c03791033d3d1468f9bef5
unknown <rebeluna263@gmail.com>
2023-08-25 18:19:40 -0300

    se agrego el componente contentRowTop en ContentWrapper

--- front/src/components/ContentWrapper.js
+++ front/src/components/ContentWrapper.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Footer from './Footer';
 import TopBar from './TopBar';
+import ContentRowTop from './ContentRowTop';
 
-function ContentWrapper() {
+function ContentWrapper(props) {
   return (
     <React.Fragment>
         <div id="content-wrapper" className="d-flex flex-column">
             <div id="content">
                 <TopBar/>
+                <ContentRowTop
+                  countProduct={props.countProduct}
+                />
                 <Footer/>
             </div>
         </div>
