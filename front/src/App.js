cd0c3af8d99d47da5257e0caf2c0c41bbfa4f8f2
unknown <rebeluna263@gmail.com>
2023-08-25 18:11:52 -0300

    se agrego el llamado de la api de products en el componente App.js

--- front/src/App.js
+++ front/src/App.js
@@ -3,13 +3,32 @@ import './App.css';
 import '../src/assets/css/app.css'
 import SideBar from './components/SideBar';
 import ContentWrapper from './components/ContentWrapper';
+import React ,{useState, useEffect} from 'react';
 
 function App() {
+  const [products,setProducts] = useState([]);
+  const [countProducts, setCountProducts]= useState(0)
+    useEffect(()=>{
+        console.log('%cse montó el componente', 'color:green');
+        fetch('http://localhost:3000/api/products')
+        .then( response => response.json())
+        .then(data =>{
+            setProducts(data.products)
+            setCountProducts(data.count)
+        })
+        .catch(error => console.error(error))
+    },[])
+    useEffect(()=>{
+      console.log('se actualizo el componente');
+    },[products])
   return (
     <>
       <div id="wrapper">
         <SideBar/>
-        <ContentWrapper/>
+        <ContentWrapper 
+          products= {products}
+          countProduct={countProducts}
+        />
       </div>
     </>
   );
