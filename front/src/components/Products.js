0f06ffbd6c6fbb4d66c37080ae5ed85b9e9ba98e
unknown <rebeluna263@gmail.com>
2023-08-25 13:08:51 -0300

    llamado de la api para products

--- front/src/components/Products.js
+++ front/src/components/Products.js
@@ -2,21 +2,38 @@ import React ,{useState, useEffect} from 'react'
 
 function Products() {
     const [products,setProducts] = useState([]);
-    
+    const [countProducts, setCountProducts]= useState(0)
     useEffect(()=>{
         console.log('%cse montó el componente', 'color:green');
         fetch('http://localhost:3000/api/products')
         .then( response => response.json())
         .then(data =>{
-            console.log(data)
+            setProducts(data.products)
+            setCountProducts(data.count)
         })
         .catch(error => console.error(error))
-    })
+    },[])
+    useEffect(()=>{
+        console.log('%cse actualizó el componente','color:yellow');
+    },[products])
     return (
         <>
             <h1>Soy el componente Productos</h1>
+            <h3>Catidad de productos: {countProducts}</h3>
             <h3>Estás en la pagina:</h3>
-            <ul></ul>
+            <ul>
+                {products.length === 0 && <p>Cargando...</p>}
+                {
+                    products.map((product,index)=>{
+                        return(
+                            <li key={product.id}>
+                                <h3>{product.name}</h3>
+                                <h4>{product.characteristics}</h4>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
         </>
     )
 }
