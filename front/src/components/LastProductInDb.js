c35e07c613d957a1a193808ff34b46fdde3e9768
unknown <rebeluna263@gmail.com>
2023-08-25 21:44:49 -0300

    se creo el componente lastProduct para renderizar el ultimo producto creado

---
+++ front/src/components/LastProductInDb.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import imagenFondo from '../assets/images/profile-user.jpeg'
+
+function LastProductInDb(props) {
+    return (
+        <>
+            <div className="col-lg-6 mb-4">
+                <div className="card shadow mb-4">
+                    <div className="card-header py-3">
+                        <h5 className="m-0 font-weight-bold text-gray-800">Last movie in Data Base</h5>
+                    </div>
+                    <div className="card-body">
+                        <div className="text-center">
+                            <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{ width: 40 + 'rem' }} src={imagenFondo} alt=" Star Wars - Mandalorian " />
+                        </div>
+                        <p>{props.name}</p>
+                        <a className="btn btn-danger" target="_blank" rel="nofollow" href="/">View movie detail</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default LastProductInDb;
