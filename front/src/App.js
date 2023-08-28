0e1ecfdb1d7c9f83c022668f0b4e3994a3afa12f
unknown <rebeluna263@gmail.com>
2023-08-28 18:14:52 -0300

    se import prodcutProvider desde context para los llamados de api de product

--- front/src/App.js
+++ front/src/App.js
@@ -4,6 +4,7 @@ import SideBar from './components/SideBar';
 import ContentWrapper from './components/ContentWrapper';
 import React, { useState, useEffect } from 'react';
 import { UserProvider } from './context/user';
+import { ProductProvider } from './context/product';
 
 console.log(process.env.REACT_APP_BACK_NAME)
 console.log(process.env.REACT_APP_BACK_PORT)
@@ -14,8 +15,8 @@ const port = process.env.REACT_APP_BACK_PORT
 const urlBase = `http://${domain}:${port}/api`;
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const [countProducts, setCountProducts] = useState(0)
+  /* const [products, setProducts] = useState([]); */
+  /* const [countProducts, setCountProducts] = useState(0) */
   const [totalPageProd, setTotalPageProd] = useState(0)
   const [lastProduct, setLastProduct] = useState([])
   useEffect(() => {
@@ -23,8 +24,8 @@ function App() {
     fetch(`${urlBase}/products`)
       .then(response => response.json())
       .then(data => {
-        setProducts(data.products)
-        setCountProducts(data.count)
+        /* setProducts(data.products) */
+        /* setCountProducts(data.count) */
         setTotalPageProd(data.totalPages)
       })
       .catch(error => console.error(error))
@@ -33,15 +34,15 @@ function App() {
     console.log('se actualizo el componente');
     fetch(`${urlBase}/products?page=${totalPageProd}`)
       .then(response => response.json())
-      .then(data =>{
+      .then(data => {
         setLastProduct(data.products)
       })
-      .catch(error=> console.error(error))
+      .catch(error => console.error(error))
   }, [totalPageProd])
 
   useEffect(() => {
-    if(lastProduct.length > 0){
-      const detailProduct= lastProduct[lastProduct.length -1]
+    if (lastProduct.length > 0) {
+      const detailProduct = lastProduct[lastProduct.length - 1]
       setLastProduct(detailProduct)
     }
   }, [lastProduct])
@@ -50,13 +51,13 @@ function App() {
     <>
       <div id="wrapper">
         <SideBar />
-        <UserProvider>
-          <ContentWrapper
-            products={products}
-            countProduct={countProducts}
-            detailProd={lastProduct}
-          />
-        </UserProvider>
+        <ProductProvider>
+          <UserProvider>
+            <ContentWrapper
+              detailProd={lastProduct}
+            />
+          </UserProvider>
+        </ProductProvider>
       </div>
     </>
   );
