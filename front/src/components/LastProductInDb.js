a7f6ba306f835b1451a61273f937b03ad6882205
unknown <rebeluna263@gmail.com>
2023-08-28 20:27:41 -0300

    se modifico el llamado a las props

--- front/src/components/LastProductInDb.js
+++ front/src/components/LastProductInDb.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import imagenFondo from '../assets/images/profile-user.jpeg'
 
-function LastProductInDb(props) {
+function LastProductInDb({detailData}) {
+    const {name, detail}=detailData;
     return (
         <>
-            <div className="col-lg-6 mb-4">
+            <div className="col-lg-4 mb-4">
                 <div className="card shadow mb-4">
                     <div className="card-header py-3">
-                        <h5 className="m-0 font-weight-bold text-gray-800">Last movie in Data Base</h5>
+                        <h5 className="m-0 font-weight-bold text-gray-800">Último Producto en la Base de Datos</h5>
                     </div>
                     <div className="card-body">
                         <div className="text-center">
                             <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{ width: 40 + 'rem' }} src={imagenFondo} alt=" Star Wars - Mandalorian " />
                         </div>
-                        <p>{props.name}</p>
-                        <a className="btn btn-danger" target="_blank" rel="nofollow" href="/">View movie detail</a>
+                        <p>{name}</p>
+                        <a className="btn bg-danger text-white"  rel="nofollow" href={detail} >View detail</a>
                     </div>
                 </div>
             </div>
