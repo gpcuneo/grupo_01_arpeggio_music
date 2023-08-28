61ccf25c32f5ea725298563af36dbb1e5ff85fb8
unknown <rebeluna263@gmail.com>
2023-08-28 20:22:13 -0300

    se elimino useEffect de product en App.js y solo llamano a ProductProvider

--- front/src/App.js
+++ front/src/App.js
@@ -2,52 +2,13 @@ import './App.css';
 import '../src/assets/css/app.css'
 import SideBar from './components/SideBar';
 import ContentWrapper from './components/ContentWrapper';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { UserProvider } from './context/user';
 import { ProductProvider } from './context/product';
 import { CategoryProvider } from './context/category';
 
-console.log(process.env.REACT_APP_BACK_NAME)
-console.log(process.env.REACT_APP_BACK_PORT)
-
-const domain = process.env.REACT_APP_BACK_NAME
-const port = process.env.REACT_APP_BACK_PORT
-
-const urlBase = `http://${domain}:${port}/api`;
 
 function App() {
-  /* const [products, setProducts] = useState([]); */
-  /* const [countProducts, setCountProducts] = useState(0) */
-  const [totalPageProd, setTotalPageProd] = useState(0)
-  const [lastProduct, setLastProduct] = useState([])
-  useEffect(() => {
-    console.log('%cse montó el componente', 'color:green');
-    fetch(`${urlBase}/products`)
-      .then(response => response.json())
-      .then(data => {
-        /* setProducts(data.products) */
-        /* setCountProducts(data.count) */
-        setTotalPageProd(data.totalPages)
-      })
-      .catch(error => console.error(error))
-  }, [])
-  useEffect(() => {
-    console.log('se actualizo el componente');
-    fetch(`${urlBase}/products?page=${totalPageProd}`)
-      .then(response => response.json())
-      .then(data => {
-        setLastProduct(data.products)
-      })
-      .catch(error => console.error(error))
-  }, [totalPageProd])
-
-  useEffect(() => {
-    if (lastProduct.length > 0) {
-      const detailProduct = lastProduct[lastProduct.length - 1]
-      setLastProduct(detailProduct)
-    }
-  }, [lastProduct])
-
   return (
     <>
       <div id="wrapper">
@@ -55,9 +16,7 @@ function App() {
         <ProductProvider>
           <UserProvider>
             <CategoryProvider>
-              <ContentWrapper
-                detailProd={lastProduct}
-              />
+              <ContentWrapper/>
             </CategoryProvider>
           </UserProvider>
         </ProductProvider>
