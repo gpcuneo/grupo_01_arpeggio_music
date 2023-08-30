2b85a200e278c62429ed70612d99ce426175141d
unknown <rebeluna263@gmail.com>
2023-08-30 00:08:43 -0300

    se agrego las funciones next y back para el paginado de listado de producto

--- front/src/context/product.js
+++ front/src/context/product.js
@@ -15,6 +15,17 @@ const getLastProduct= async(lastPage)=>{
     const lastProductInfo= lastProductDetail.product;
     return lastProductInfo;
 }
+
+const next =async (page,totalPages)=>{
+  let nextPage = page === totalPages? totalPages:page + 1;
+  const productsListByPage = await apiCall({url:`${urlBase}/products?page=${nextPage}`});
+  return productsListByPage;
+}
+const back = async (page)=>{
+  let pageback= page > 0 ? page-1 : 1;
+  const productsListByPage = await apiCall({url:`${urlBase}/products?page=${pageback}`});
+  return productsListByPage;
+}
 // Crea el contexto
 const ProductContext=createContext('products');
 
@@ -23,21 +34,29 @@ export function ProductProvider({children}) {
     const [products, setProducts]=useState([]);
     const [lastProduct, setLastProduct]=useState([]);
     const [isLoadingPro, setIsLoadingPro]=useState(true);
-
+    const [productsFirst, setProductsFirst]= useState([])
+    const [productPage, setProductPage]=useState(1);
     useEffect(()=>{
       //Función para realizar la llamada a la API
+      const getProductList = async()=>{
+        const ProductListData= await getProducts();
+        setProductsFirst(ProductListData)
+      } 
       const getProductData= async()=>{
         try {
+          //Indica que se estan cargando datos
           setIsLoadingPro(true)
+          //Le setea la api de Productos
           const productsData= await getProducts()
           setProducts(productsData);
+          //Le setea el último producto creado en la base de datos 
           let lastProductData = await getLastProduct(productsData.totalPages);
           setLastProduct(lastProductData);
           const id = lastProductData.id;
           lastProductData.img = lastProductData.image['1'];
           lastProductData.detail = `http://${domain}:${port}/products/${id}` 
-          console.log(' ------------------ lastProductData')
-          console.log(lastProductData)
+          
+          //Indica que termino de cargar todos los datos
           setIsLoadingPro(false);
         } catch (error) {
           console.error(`Error al cargar datos:`, error);
@@ -48,12 +67,12 @@ export function ProductProvider({children}) {
       }
       getProductData();
       const intervalId= setInterval(getProductData,5000); //Llama a getProductData(llama a la api cada 5 segundos)
+      getProductList();      
       
       return ()=> clearInterval(intervalId);//Limpia el intervalo cuando se desmonta el componente
     },[])
-    
   return (
-    <ProductContext.Provider value={{products,lastProduct,isLoadingPro}}>
+    <ProductContext.Provider value={{products,lastProduct,isLoadingPro, next,back,productsFirst,productPage,setProductPage,setProductsFirst}}>
       {children}
     </ProductContext.Provider>
   );
