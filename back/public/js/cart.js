5aac62e97f074130189cef7c562011670bd48762
unknown <rebeluna263@gmail.com>
2023-09-03 20:51:21 -0300

    se agrego la funcion de update del carrito

--- back/public/js/cart.js
+++ back/public/js/cart.js
@@ -1,12 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const btnPlus = Array.from(document.querySelectorAll('.increment'))
-    const btnMinus = Array.from(document.querySelectorAll('.decrement'))
     const priceSend = document.querySelector('#price-send-product')
     const totalPriceShop = document.querySelector('#total-price-shop')
     const subTotal = document.querySelector('#Sub-price-product')
     const containerArticle = document.querySelector('.container-article-all')
     const url = `/api/cart`;
-    console.log(btnPlus);
     const getApiCart = async () => {
         const getCart = await fetch(url, {
             method: 'GET',
@@ -19,11 +16,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const drawCartStatus = async (dataCart) => {
         const cart = await dataCart;
         const dataProducts = await cart.products;
-        
-        const createArticle = async (product)=>{
+
+        const createArticle = async (product) => {
             const img = await JSON.parse(product.image)
             const image = img[0]
-            
+
             const article = document.createElement('article')
             article.innerHTML = `
             <div class="box-img-text">
@@ -39,9 +36,9 @@ document.addEventListener('DOMContentLoaded', function () {
         </div>
         <div class="container-input-price">
             <div class="box-input-inc-dec">
-                <button class="decrement" id="btn-decrement" > - </button>
-                <p class="number count-product">${product.quantity}</p>
-                <button class="increment" id="btn-increment"> + </button>
+                <button class="decrement" id="btn-decrement" value="${product.id}"> - </button>
+                <p class="number count-product-${product.id}">${product.quantity}</p>
+                <button class="increment" id="btn-increment" value="${product.id}"> + </button>
             </div>
             <div class="box-price-delete">
                 <p class="total-price-product">$${product.totalPriceProduct}</p>
@@ -51,41 +48,98 @@ document.addEventListener('DOMContentLoaded', function () {
             `
             containerArticle.appendChild(article)
         }
-
-        for( const product of dataProducts) {
+        for (const product of dataProducts) {
             createArticle(product)
         };
         subTotal.innerText = `$${cart.totalPrice}`
         const costSend = priceSend.innerText;
-        const valueNumber = parseFloat(costSend.replace(/\$/g, ''))  
+        const valueNumber = parseFloat(costSend.replace(/\$/g, ''))
         const total = valueNumber + cart.totalPrice;
         totalPriceShop.innerText = `$${total}`;
         return containerArticle;
     }
-    const sendUpdateOfProducts = (productId, action) => {
-        
+    const getApiProduct = async (id) => {
+        const data = await fetch(`/api/products/${id}`)
+        const detail = await data.json();
+        let product = await detail.product;
+        return product;
     }
-    const deleteProductCart = async() => {
-        containerArticle.addEventListener('click', async(e)=>{
-            if(e.target.classList.contains('delete')){
-                const id= e.target.value;
+    const apiUpdate = async (body) => {
+        try {
+            const response = await fetch(url, {
+                method: 'PUT',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body)
+            })
+            if (response.ok) {
+                const dataSend = await response.json();
+                console.log(`El producto ${dataSend} se actualizo correctamente`);
+                drawCartUpdateStatus();
+            } else {
+                console.error(`No se pudo actualizar el producto`);
+            }
+        } catch (error) {
+            console.error(`Error al actualizar el producto :${error}`);
+        }
+    }
+    const sendUpdateOfProducts = async () => {
+        containerArticle.addEventListener('click', async (e) => {
+            if (e.target.classList.contains('increment')) {
+                const id = e.target.value;
+                const newQuantity = containerArticle.querySelector(`.count-product-${id}`)
+                const product = await getApiProduct(id);
+                const stock = await product.stock;
+                let quantity = newQuantity.innerText;
+                if (quantity < stock) {
+                    newQuantity.innerHTML++
+                    let newUpdateQuantity = newQuantity.innerText
+                    let body = {
+                        quantity:newUpdateQuantity,
+                        productid:id.toString()
+                    };
+                    apiUpdate(body)
+                }
+            } else if (e.target.classList.contains('decrement')) {
+                const id = e.target.value;
+                const newQuantity = containerArticle.querySelector(`.count-product-${id}`)
+                let quantity = newQuantity.innerText;
+                if (quantity > 1) {
+                    newQuantity.innerHTML--
+                    let newUpdateQuantity = newQuantity.innerText
+                    let body = {
+                        quantity:newUpdateQuantity,
+                        productid:id.toString()
+                    };
+                    apiUpdate(body)
+                }
+            }
+        })
+    }
+    const deleteProductCart = async () => {
+        containerArticle.addEventListener('click', async (e) => {
+            if (e.target.classList.contains('delete')) {
+                const id = e.target.value;
                 let body = {
-                    productid:id.toString(),
+                    productid: id.toString(),
                 }
                 console.log(`estas haciendo click a : ${id}`);
                 try {
-                    const response = await fetch(url,{
-                        method:'DELETE',
-                        credentials:'include',
-                        headers:{
+                    const response = await fetch(url, {
+                        method: 'DELETE',
+                        credentials: 'include',
+                        headers: {
                             'Content-Type': 'application/json'
                         },
                         body: JSON.stringify(body)
                     })
-                    if(response.ok){
+                    if (response.ok) {
                         const dataSend = await response.json();
                         console.log(`El producto ${dataSend} se elimino correctamente`);
-                    }else{
+                        drawCartUpdateStatus();
+                    } else {
                         console.error(`No se pudo eliminar el producto`);
                     }
                 } catch (error) {
@@ -95,9 +149,10 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     }
     const drawCartUpdateStatus = async () => {
-        containerArticle.innerHTML= ''
+        containerArticle.innerHTML = ''
         const dataProductCart = await getApiCart();
         const datos = await drawCartStatus(dataProductCart)
+        sendUpdateOfProducts(dataProductCart)
         deleteProductCart();
         return datos
     }
